6debc71dad55ce37bb5e007db9ae62f0003a4d04
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 12:32:43 -0700

    beginning mongo refactor

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,9 +1,9 @@
 // NOTE: this file is not needed when using MongoDB
-var db = require('../config');
-var User = require('../models/user');
+// var db = require('../models/users');
+var Users = require('../models/user');
 
-var Users = new db.Collection();
+// var Users = new db.Collection();
 
-Users.model = User;
+// Users.model = User;
 
 module.exports = Users;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
 
+mongoose.connect('mongodb://localhost/shortly');
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
